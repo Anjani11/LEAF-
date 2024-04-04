d64c4c181a377ff2a4875e2c95171faa0f4f21e9
KATAKAM ANJANI AKANKSHA <112406462+Anjani11@users.noreply.github.com>
2024-04-04 15:18:07 +0530

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,31 +1,70 @@
+from __future__ import annotations
+
+import logging
 import os
 import sys
-import warnings
+from timeit import default_timer
 
-# Remove '' and current working directory from the first entry
-# of sys.path, if present to avoid using current directory
-# in pip commands check, freeze, install, list and show,
-# when invoked as python -m pip <command>
-if sys.path[0] in ("", os.getcwd()):
-    sys.path.pop(0)
 
-# If we are running from a wheel, add the wheel to sys.path
-# This allows the usage python pip-*.whl/pip install pip-*.whl
-if __package__ == "":
-    # __file__ is pip-*.whl/pip/__main__.py
-    # first dirname call strips of '/__main__.py', second strips off '/pip'
-    # Resulting path is the name of the wheel itself
-    # Add that to sys.path so we can import pip
-    path = os.path.dirname(os.path.dirname(__file__))
-    sys.path.insert(0, path)
+def run(args=None, options=None, env=None):
+    env = os.environ if env is None else env
+    start = default_timer()
+    from virtualenv.run import cli_run  # noqa: PLC0415
+    from virtualenv.util.error import ProcessCallFailedError  # noqa: PLC0415
 
-if __name__ == "__main__":
-    # Work around the error reported in #9540, pending a proper fix.
-    # Note: It is essential the warning filter is set *before* importing
-    #       pip, as the deprecation happens at import time, not runtime.
-    warnings.filterwarnings(
-        "ignore", category=DeprecationWarning, module=".*packaging\\.version"
-    )
-    from pip._internal.cli.main import main as _main
+    if args is None:
+        args = sys.argv[1:]
+    try:
+        session = cli_run(args, options, env)
+        logging.warning(LogSession(session, start))
+    except ProcessCallFailedError as exception:
+        print(f"subprocess call failed for {exception.cmd} with code {exception.code}")  # noqa: T201
+        print(exception.out, file=sys.stdout, end="")  # noqa: T201
+        print(exception.err, file=sys.stderr, end="")  # noqa: T201
+        raise SystemExit(exception.code)  # noqa: TRY200, B904
 
-    sys.exit(_main())
+
+class LogSession:
+    def __init__(self, session, start) -> None:
+        self.session = session
+        self.start = start
+
+    def __str__(self) -> str:
+        spec = self.session.creator.interpreter.spec
+        elapsed = (default_timer() - self.start) * 1000
+        lines = [
+            f"created virtual environment {spec} in {elapsed:.0f}ms",
+            f"  creator {self.session.creator!s}",
+        ]
+        if self.session.seeder.enabled:
+            lines.append(f"  seeder {self.session.seeder!s}")
+            path = self.session.creator.purelib.iterdir()
+            packages = sorted("==".join(i.stem.split("-")) for i in path if i.suffix == ".dist-info")
+            lines.append(f"    added seed packages: {', '.join(packages)}")
+
+        if self.session.activators:
+            lines.append(f"  activators {','.join(i.__class__.__name__ for i in self.session.activators)}")
+        return "\n".join(lines)
+
+
+def run_with_catch(args=None, env=None):
+    from virtualenv.config.cli.parser import VirtualEnvOptions  # noqa: PLC0415
+
+    env = os.environ if env is None else env
+    options = VirtualEnvOptions()
+    try:
+        run(args, options, env)
+    except (KeyboardInterrupt, SystemExit, Exception) as exception:
+        try:
+            if getattr(options, "with_traceback", False):
+                raise
+            if not (isinstance(exception, SystemExit) and exception.code == 0):
+                logging.error("%s: %s", type(exception).__name__, exception)  # noqa: TRY400
+            code = exception.code if isinstance(exception, SystemExit) else 1
+            sys.exit(code)
+        finally:
+            logging.shutdown()  # force flush of log messages before the trace is printed
+
+
+if __name__ == "__main__":  # pragma: no cov
+    run_with_catch()  # pragma: no cov
